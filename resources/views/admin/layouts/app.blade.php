6bc0bd1d32d1938da297da5086b485bc7e2eb373
Ahmed Sarmad <ahmedalhaddawi@yahoo.com>
2023-11-17 05:18:58 +0200

    vsersion 2

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -261,7 +261,7 @@
         <strong>Copyright &copy; 2023 <a href="https://www.anzemah.com">Anzemah</a>.</strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.2.3
+        <b>Version</b> 2.0
         </div>
     </footer>
     <!-- Control Sidebar -->
